1f235674d2505d8e2684778b8da6d95ec1e4913e
tracilevine <televine@gmail.com>
2020-10-16 11:58:34 -0400

    added post and get options

--- server.js
+++ server.js
@@ -25,6 +25,31 @@ app.get("/", (req, res) => {
     res.status(200).send("TO DO App");
   });
 
+  // this route will return all of the existing TO DO items
+  const items = [];
+  let id = 0;
+
+  app.get("/api/items", (req, res, next) => {
+    res.json(items);
+  });
+
+// this route will create a new to do item
+
+app.post("/api/items", (req, res, next) => {
+  if (req.body.item) {
+    id = id + 1;
+    const newItem = {
+      id: id,
+      item: req.body.item,
+      completed: false,
+    };
+    items.push(newItem);
+    res.json(newItem);
+  } else {
+    res.status(400).json({ error: "item needs a description" });
+  }
+});
+
 /**
  * Server Activation
  */
